181b5f43b9f762278b0bfe1b31c6c8ff4af3f108
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-15 15:48:48 +0530

    cicd test for user service

--- src/main/java/com/sliit/betterwellness/controller/CounsellorController.java
+++ src/main/java/com/sliit/betterwellness/controller/CounsellorController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/better-wellness/user")
+@RequestMapping("/user-service")
 @Slf4j
 public class CounsellorController {
 
