9ee790a1a85fad328e523d315d09cbe9808787ef
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-21 13:22:14 +0530

    added logs

--- src/main/java/com/sliit/betterwellness/controller/CounsellorController.java
+++ src/main/java/com/sliit/betterwellness/controller/CounsellorController.java
@@ -5,6 +5,8 @@ import com.sliit.betterwellness.dto.CounsellorsDTO;
 import com.sliit.betterwellness.service.CounsellorService;
 import com.sliit.betterwellness.utills.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +20,7 @@ import java.util.Map;
 @Slf4j
 public class CounsellorController {
 
-
+	private static final Logger log = LogManager.getLogger(CounsellorController.class);
 	private final CounsellorService counsellorService;
 
 	@Autowired
@@ -28,27 +30,34 @@ public class CounsellorController {
 
 	@PostMapping("/counsellor")
 	public ResponseEntity<CounsellorDTO> updateUser(@RequestBody CounsellorDTO counsellorDTO, @RequestHeader Map<String, String> headers){
-	//	log.info("Counsellor save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(counsellorService.saveCounsellor(counsellorDTO));
+		log.info("[IN] Counsellor save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		CounsellorDTO response = counsellorService.saveCounsellor(counsellorDTO);
+		log.info("[OUT] Counsellor save request correlation-id : {}, Request {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorDTO, response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 	@GetMapping("/counsellor")
 	public ResponseEntity<CounsellorsDTO> searchUser(@RequestParam(name = "search", required = false, defaultValue = "") String search, @RequestHeader Map<String, String> headers){
-	//	log.info("Counsellor search request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		log.info("[IN] Counsellor search request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
 		CounsellorsDTO response = new CounsellorsDTO();
 		response.setCounsellors(counsellorService.searchCounsellor(search));
+		log.info("[OUT] Counsellor search request correlation-id : {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), response);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@GetMapping("/counsellor/{counsellorId}")
 	public ResponseEntity<CounsellorDTO> searchUser(@PathVariable int counsellorId, @RequestHeader Map<String, String> headers){
-	//	log.info("Counsellor get request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.OK).body(counsellorService.getCounsellor(counsellorId));
+		log.info("[IN] Counsellor get request correlation-id : {}, counsellorId {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId);
+		CounsellorDTO response = counsellorService.getCounsellor(counsellorId);
+		log.info("[OUT] Counsellor get request correlation-id : {}, counsellorId {}, response {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId, response);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@DeleteMapping("/counsellor/{counsellorId}")
 	public ResponseEntity<CounsellorDTO> deleteUser(@PathVariable int counsellorId, @RequestHeader Map<String, String> headers){
-	//	log.info("Counsellor delete request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(counsellorService.deleteCounsellor(counsellorId));
+		log.info("[IN] Counsellor delete request correlation-id : {}, counsellorId {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId);
+		CounsellorDTO response = counsellorService.deleteCounsellor(counsellorId);
+		log.info("[OUT] Counsellor delete request correlation-id : {}, counsellorId {}", headers.get(Constants.HEADER_CORRELATION_ID), counsellorId);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
--- src/main/java/com/sliit/betterwellness/controller/CustomerController.java
+++ src/main/java/com/sliit/betterwellness/controller/CustomerController.java
@@ -4,6 +4,8 @@ import com.sliit.betterwellness.dto.CustomerDTO;
 import com.sliit.betterwellness.service.CustomerService;
 import com.sliit.betterwellness.utills.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +19,7 @@ import java.util.Map;
 @Slf4j
 public class CustomerController {
 
-
+	private static final Logger logger = LogManager.getLogger(CustomerController.class);
 	private final CustomerService customerService;
 
 	@Autowired
@@ -27,25 +29,33 @@ public class CustomerController {
 
 	@PostMapping("/customer")
 	public ResponseEntity<CustomerDTO> updateUser(@RequestBody CustomerDTO customerDTO, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(customerService.saveCustomer(customerDTO));
+		logger.info("[IN] Customer save request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		CustomerDTO response = customerService.saveCustomer(customerDTO);
+		logger.info("[OUT] Customer save request correlation-id : {}, Request {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), customerDTO, response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 	@GetMapping("/customer")
 	public ResponseEntity<List<CustomerDTO>> searchUser(@RequestParam(name = "search", required = false, defaultValue = "") String search, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer search request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.OK).body(customerService.searchCustomer(search));
+		logger.info("[IN] Customer search request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
+		List<CustomerDTO> response = customerService.searchCustomer(search);
+		logger.info("[OUT] Customer search request correlation-id : {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), response);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@GetMapping("/customer/{customerId}")
 	public ResponseEntity<CustomerDTO> searchUser(@PathVariable int customerId, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer get request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomer(customerId));
+		logger.info("[IN] Customer get request correlation-id : {}, customerId {}", headers.get(Constants.HEADER_CORRELATION_ID), customerId);
+		CustomerDTO response = customerService.getCustomer(customerId);
+		logger.info("[OUT] Customer search request correlation-id : {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), response);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@DeleteMapping("/customer/{customerId}")
 	public ResponseEntity<CustomerDTO> deleteUser(@PathVariable int customerId, @RequestHeader Map<String, String> headers){
-	//	log.info("Customer delete request correlation-id : {}", headers.get(Constants.HEADER_CORRELATION_ID));
-		return ResponseEntity.status(HttpStatus.CREATED).body(customerService.deleteCustomer(customerId));
+		logger.info("[IN] Customer delete request correlation-id : {}, customerId {}", headers.get(Constants.HEADER_CORRELATION_ID), customerId);
+		CustomerDTO response = customerService.deleteCustomer(customerId);
+		logger.info("[OUT] Customer delete request correlation-id : {}, Response {}", headers.get(Constants.HEADER_CORRELATION_ID), response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
